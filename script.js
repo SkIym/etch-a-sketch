0861b5303301e11f0a72a32d74157e7c9ade0e5c
SkIym <abramjosh13@gmail.com>
2023-07-17 18:22:59 +0800

    Add size range. Change grid container to actual grid display for smooth transition.

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
+// Global, default values
+
+const DEFSIZE = 16;
+const gridContainer = document.getElementById("grid-container")
+
+// When grid size is toggled by the user 
+const sizeRange = document.getElementById("setSquareNumber");
+sizeRange.onchange = (e) => resizeGrid(e.target.value);
 
 
-
-// Create the grid squares, add listeners
-
-const gridContainer = document.querySelector(".grid-container")
-
-for (let s = 0; s < 16*16; s+=1) {
-    const square = document.createElement("div");
-    square.style.width = "30px";
-    square.style.height = "30px";
-    square.style.backgroundColor = "red";
-
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
-    });
-
-    gridContainer.appendChild(square);
-
+function clearGrid() {
+    gridContainer.innerHTML = "";
 }
 
+function resizeGrid(size) {
+    clearGrid()
+    setGrid(size)
+}
+
+// Credit: michalosman, TOP
+let toggleLMB = false
+document.body.onmousedown = () => (toggleLMB = true)
+document.body.onmouseup = () => (toggleLMB = false)
+
+function changeColor(e) {
+    const square = e.target;
+    // hover + LMB
+    if (e.type === 'mouseover' && toggleLMB) {
+        square.style.backgroundColor = "black";
+    }
+}
+
+// Create the grid, add listeners
+
+
+function setGrid(size=16) {
+
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let s = 0; s < size*size; s+=1) {
+        const square = document.createElement("div");
+        square.classList.add("grid-item");
+        square.addEventListener("mouseover", changeColor);
+        gridContainer.appendChild(square);
+    
+    }
+}
+
+// When site loads
+window.onload = () => {
+    setGrid(DEFSIZE);
+}
